5ec87246aac393987ffc0da0c9bbbfd5b41e579f
SheilaLeón <SheilaLen@T20211>
2018-10-30 08:55:22 +0000

    Version 1.1

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -1,5 +1,20 @@
 package ejercicios;
 
 public class Ejercicios {
+	 public float calcularSaldoFinal (float saldoInicial,float[] movimientos) {
+		return saldoInicial;
+		 
+	 }
+	 
+	 public int ejercicio2(int n, int inferior, int superior) {
+		return superior;
+		 
+	 }
+	 
+	 
+	 
+	 public static void main(String[] args) {
+		 
+	 }
 
 }
