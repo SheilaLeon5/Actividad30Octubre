fee438c6363f50dd60c1e4c6b3ce89737e18e2bf
sheila <sheila@Len>
2018-10-30 21:40:08 +0000

    Version 3. - Actividad Realizada con éxito (Acti1 y Act2).

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -5,13 +5,16 @@ import java.util.Random;
 
 /**
  * @author Sheila León Santana
+ *  30/10/2018
  *
  */
 public class Ejercicios {
 	public float calcularSaldoFinal(float saldoInicial, float[] movimientos) {
-		
-		return saldoInicial;
-
+		int saldofin = 0;
+		for (int i = 0; i < movimientos.length; i++) {
+			saldofin += movimientos[i]; 
+		}
+		return saldoInicial + saldofin;
 	}
 
 	public int ejercicio2(int n, int inferior, int superior) {
@@ -26,14 +29,26 @@ public class Ejercicios {
 	}
 
 	public static void main(String[] args) {
-		//Ejercicios ejercicios = new Ejercicios();
-		//int media = ejercicios.ejercicio2(5, 1, 100);
-		//System.out.println(media);
 		
-		
+		/* -- Actividad 1 -- Dado un saldo inicial y una lista de movimientos, se pide obtener el saldo final.
+		 * 
+		 */
 		Ejercicios ejercicios = new Ejercicios();
-		int media = ejercicios.calcularSaldoFinal(100, movimientos);
-		float [] movimienots = new float[]
+		float [] movimientos = {100,-50,2,-8};
+		float  calcSaldo = ejercicios.calcularSaldoFinal(100, movimientos);
+		System.out.println("Saldo Actual de su cuenta: " + calcSaldo + "€");
+		
+		
+		
+		
+		/* -- Actividad 2--   obtener un método que devuelva la media de  n números aleatorios.
+		   
+		 Ejercicios ejercicios = new Ejercicios();
+		 int media = ejercicios.ejercicio2(5, 1, 100);
+		 System.out.println(media);
+		*/
+				
+		
 	}
 
 }
