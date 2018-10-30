3c04668f3b34e6ec79c8d995f26e622bff72f178
SheilaLeón <SheilaLen@T20211>
2018-10-30 09:43:46 +0000

    Version 2. - Actividad Realizada. (Falta Act1)

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -1,7 +1,12 @@
+
 package ejercicios;
 
 import java.util.Random;
 
+/**
+ * @author Sheila León Santana
+ *
+ */
 public class Ejercicios {
 	public float calcularSaldoFinal(float saldoInicial, float[] movimientos) {
 		
@@ -17,14 +22,18 @@ public class Ejercicios {
 			numero = inferior + rnd.nextInt(superior);
 			acumulador += numero;
 		}
-
 		return acumulador / n;
 	}
 
 	public static void main(String[] args) {
+		//Ejercicios ejercicios = new Ejercicios();
+		//int media = ejercicios.ejercicio2(5, 1, 100);
+		//System.out.println(media);
+		
+		
 		Ejercicios ejercicios = new Ejercicios();
-		int media = ejercicios.ejercicio2(5, 1, 100);
-		System.out.println(media);
+		int media = ejercicios.calcularSaldoFinal(100, movimientos);
+		float [] movimienots = new float[]
 	}
 
 }
