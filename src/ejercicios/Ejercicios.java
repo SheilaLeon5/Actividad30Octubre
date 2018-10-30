6332791dff53b58c42c5c57e5bfa08033d170483
SheilaLeón <SheilaLen @T20211>
2018-10-30 09:19:20 +0000

    Version 2 - Actividad 2 realizada (falta actividad 1)

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -1,20 +1,30 @@
 package ejercicios;
 
+import java.util.Random;
+
 public class Ejercicios {
-	 public float calcularSaldoFinal (float saldoInicial,float[] movimientos) {
+	public float calcularSaldoFinal(float saldoInicial, float[] movimientos) {
+		
 		return saldoInicial;
-		 
-	 }
-	 
-	 public int ejercicio2(int n, int inferior, int superior) {
-		return superior;
-		 
-	 }
-	 
-	 
-	 
-	 public static void main(String[] args) {
-		 
-	 }
+
+	}
+
+	public int ejercicio2(int n, int inferior, int superior) {
+		int numero;
+		int acumulador = 0;
+		Random rnd = new Random();
+		for (int i = 0; i < n; i++) {
+			numero = inferior + rnd.nextInt(superior);
+			acumulador += numero;
+		}
+
+		return acumulador / n;
+	}
+
+	public static void main(String[] args) {
+		Ejercicios ejercicios = new Ejercicios();
+		int media = ejercicios.ejercicio2(5, 1, 100);
+		System.out.println(media);
+	}
 
 }
